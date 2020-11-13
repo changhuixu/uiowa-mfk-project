ca7343d034a5860a527eed9352ecec5ebeea0dab
Changhui Xu <changhuixu@hotmail.com>
2020-11-13 10:54:39 -0600

    add another test

--- cypress/integration/mfk-input-component-tests/advanced-demos.spec.ts
+++ cypress/integration/mfk-input-component-tests/advanced-demos.spec.ts
@@ -17,7 +17,7 @@ describe('Advanced Demos', () => {
       .should('be.equal', '020-12-1012-01001-00000000-xxxx-000-00111-12-3555');
   });
 
-  it.only('should allow to set options', () => {
+  it('should allow to set options', () => {
     cy.get('#checkbox-readonly').click();
     cy.get('#checkbox-brf').click();
     cy.get('#set-option2-btn').click();
@@ -46,5 +46,44 @@ describe('Advanced Demos', () => {
     ).then(($el) => {
       expect($el.length).to.be.equal(11);
     });
+  });
+
+  it('should update existing MFK after set options', () => {
+    const dt = new DataTransfer();
+    dt.setData('text/plain', '0201210120100100000000621900000111123555');
+    const pasteEvent = new ClipboardEvent('paste', {
+      clipboardData: dt,
+      bubbles: true,
+      cancelable: true,
+    });
+
+    cy.get(
+      '#configurable-options > uiowa-mfk-input > #mfk-container > :nth-child(2) > .form-control'
+    ).then((inputs) => {
+      expect(inputs.length).to.be.equal(1);
+      inputs[0].dispatchEvent(pasteEvent);
+    });
+    cy.get(
+      '#configurable-options > uiowa-mfk-input > #mfk-container > :nth-child(6) > .form-control'
+    ).should('have.value', '6219');
+
+    cy.get('#set-option2-btn').click();
+
+    cy.get('pre').then(($el) => {
+      const json = JSON.parse($el[0].textContent || '');
+      expect(json.length).to.be.equal(1);
+      expect(json[0].name).to.be.equal('iact');
+      expect(json[0].defaultValue).to.be.equal('6218');
+      expect(json[0].readonly).to.be.equal(false);
+    });
+    cy.get('#mfk2-string > uiowa-mfk-string')
+      .invoke('text')
+      .should('be.equal', '020-12-1012-01001-00000000-6218-000-00111-12-3555');
+
+    cy.get(
+      '#configurable-options > uiowa-mfk-input > #mfk-container > :nth-child(6) > .form-control'
+    )
+      .should('have.value', '6218')
+      .should('not.have.attr', 'readonly');
   });
 });
